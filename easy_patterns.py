160930d535f64ba1c7de6044e1c072205e2f1686
file-not-found <file-not-found@tempmail.de>
2017-10-11 21:45:38 +0200

    wordlists added

--- easy_patterns.py
+++ easy_patterns.py
@@ -3,13 +3,15 @@ from math import floor, ceil
 min_len = 1
 max_len = 20
 charsets = {
-    'numbers'   : '1234567890',
+    'numbers'   : '1234567890-=',
+    'numbersg'  : '1234567890ß´',
     'letters'   : 'abcdefghijklmnopqrstuvwxyz',
     'keyboard0' : '!@#$%^&*()_+',
     'keyboard0g': '!"§$%&/()=?`',
     'keyboard1' : 'qwertyuiop[]',
-    'keyboard1g': 'qwertzuiop',
+    'keyboard1g': 'qwertzuiopü+',
     'keyboard2' : 'asdfghjkl;\'\\',
+    'keyboard2g': 'asdfghjklöä#',
     'keyboard3' : 'zxcvbnm,./',
     'keyboard3g': 'yxcvbnm,.-',
 }
@@ -18,7 +20,7 @@ for cs in charsets.values():
         for repeat in range(ceil(min_len/end),floor(max_len/end)+1):
             print(cs[:end]*repeat)
 
-cs = 'abcdefghijklmnopqrstuvwxyz0123456789!"§#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+cs = 'abcdefghijklmnopqrstuvwxyz0123456789!"§#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~öaüß'
 for c in cs:
     for length in range(min_len, max_len+1):
         print(c*length)
