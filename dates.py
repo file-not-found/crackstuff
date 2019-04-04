b8d020db455c654203022d424a2d6b49ca434795
file-not-found <file-not-found@tempmail.de>
2019-04-04 07:21:23 +0200

    wordlist reorganisation

--- dates.py
+++ dates.py
@@ -10,6 +10,7 @@ parser.add_argument("-f", "--format", type=int, choices=[1,2,3], default=3,
     help="1=DDMMYYYY, 2=MMDDYYYY, 3=both")
 parser.add_argument("-d", "--delimiter", type=str, default='',
     help="delimiter between day, month and year")
+parser.add_argument("-r","--rules", default=False, action="store_true", help="generate as hashcat rules")
 args=parser.parse_args()
 
 start=args.start
@@ -21,6 +22,14 @@ for year in range(start,end):
     for month in range(1,13):
         for day in range(1,32):
             if f == 1 or f == 3:
-                print("%02d%s%02d%s%d" % (day, d, month, d, year))
+                date=("%02d%s%02d%s%d" % (day, d, month, d, year))
+                if args.rules:
+                    print('$' + ' $'.join(date))
+                else:
+                    print(date)
             if f > 1:
-                print("%02d%s%02d%s%d" % (month, d, day, d, year))
+                date=("%02d%s%02d%s%d" % (month, d, day, d, year))
+                if args.rules:
+                    print('$' + ' $'.join(date))
+                else:
+                    print(date)
